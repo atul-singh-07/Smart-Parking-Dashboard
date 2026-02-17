5a318899d32017ef4d8089c72231abe8b9650be3
Atul Singh <satul1628@gmail.com>
2026-02-17 20:39:05 +0530

    Update Firebase configuration with new credentials

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,13 +3,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyDPPCKJM6WQvsVFisQ5SbqBgvWuSt9AYtQ",
+  authDomain: "smart-parking-system-1cf2a.firebaseapp.com",
+  databaseURL: "https://smart-parking-system-1cf2a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "smart-parking-system-1cf2a",
+  storageBucket: "smart-parking-system-1cf2a.firebasestorage.app",
+  messagingSenderId: "1006250338223",
+  appId: "1:1006250338223:web:fd1d57aadeb59c4827315a"
 };
 
 const app = initializeApp(firebaseConfig);
