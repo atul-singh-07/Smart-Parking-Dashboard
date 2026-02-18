db01e33d198ef6fb910f8bd5896db1b429ae23f4
Atul Singh <satul1628@gmail.com>
2026-02-18 19:02:43 +0530

    Add dummySlots array to dummyLocations.js

--- src/data/dummyLocations.js
+++ src/data/dummyLocations.js
@@ -1,1 +1,10 @@
-
+export const dummySlots = [
+  { id: 1, occupied: false },
+  { id: 2, occupied: true },
+  { id: 3, occupied: false },
+  { id: 4, occupied: true },
+  { id: 5, occupied: false },
+  { id: 6, occupied: false },
+  { id: 7, occupied: true },
+  { id: 8, occupied: false }
+];
