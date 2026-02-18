80c09203d1cec0ae81219dcdcb18e2d180e00ac9
Atul Singh <satul1628@gmail.com>
2026-02-18 19:31:26 +0530

    Add dummy locations data with slots information

--- data/dummyLocations.js
+++ data/dummyLocations.js
@@ -1,1 +1,16 @@
-
+export const locations = [
+  {
+    id:"sec21",
+    name:"Sector 21 Metro Station",
+    slots:[
+      {id:1, occupied:false},
+      {id:2, occupied:true},
+      {id:3, occupied:false},
+      {id:4, occupied:false},
+      {id:5, occupied:true},
+      {id:6, occupied:false},
+      {id:7, occupied:false},
+      {id:8, occupied:true}
+    ]
+  }
+];
