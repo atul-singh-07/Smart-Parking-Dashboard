39b032484d54fef39cf251fc7bec4742a8f21335
Atul Singh <satul1628@gmail.com>
2026-02-18 19:57:27 +0530

    Delete js directory

--- js/ui/counter.js
+++
@@ -1,6 +0,0 @@
-export function updateCounter(slots){
-  const counter=document.getElementById("counter");
-  const available=slots.filter(s=>!s.occupied).length;
-
-  counter.textContent=`Available Slots: ${available}`;
-}
