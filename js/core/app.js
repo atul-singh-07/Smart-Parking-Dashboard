291cfc5c0f00052c9b380c2a32de3fd4d4b7a263
Atul Singh <satul1628@gmail.com>
2026-02-18 19:33:06 +0530

    Update app.js

--- js/core/app.js
+++ js/core/app.js
@@ -1,1 +1,19 @@
+import { locations } from "../../data/dummyLocations.js";
+import { renderSidebar } from "../ui/sidebar.js";
+import { renderMap } from "../modules/mapRenderer.js";
+import { updateCounter } from "../ui/counter.js";
 
+let currentLocation=locations[0];
+
+function render(){
+  document.getElementById("locationTitle").textContent=currentLocation.name;
+  renderMap(currentLocation.slots);
+  updateCounter(currentLocation.slots);
+}
+
+renderSidebar(locations,(id)=>{
+  currentLocation=locations.find(l=>l.id===id);
+  render();
+});
+
+render();
